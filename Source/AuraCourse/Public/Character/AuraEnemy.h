587bda0e8c069d91f419c7f57602aa714c6566d5
MatthewG <Matthewgraham375@gmail.com>
2025-05-09 13:02:16 -0400

    Enemy Interface

--- Source/AuraCourse/Public/Character/AuraEnemy.h
+++ Source/AuraCourse/Public/Character/AuraEnemy.h
@@ -4,14 +4,20 @@
 
 #include "CoreMinimal.h"
 #include "Character/AuraCharacterBase.h"
+#include "Interaction/EnemyInterface.h"
 #include "AuraEnemy.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class AURACOURSE_API AAuraEnemy : public AAuraCharacterBase
+class AURACOURSE_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface
 {
 	GENERATED_BODY()
+
+public:
+	virtual void HighlightActor() override;
+	virtual void UnHighlightActor() override;
+	
 	
 };
