808403f122972f70174c6f52235cd647508e8837
MatthewG <Matthewgraham375@gmail.com>
2025-05-14 14:08:25 -0400

    Post Process Highlight

--- Source/AuraCourse/Private/Character/AuraEnemy.cpp
+++ Source/AuraCourse/Private/Character/AuraEnemy.cpp
@@ -3,12 +3,27 @@
 
 #include "Character/AuraEnemy.h"
 
+#include "AuraCourse/AuraCourse.h"
+
+AAuraEnemy::AAuraEnemy()
+{
+	GetMesh()->SetCollisionResponseToChannel(ECC_Visibility, ECR_Block);
+}
+
 void AAuraEnemy::HighlightActor()
 {
-	bDrawDebug = true;
+	GetMesh()->SetRenderCustomDepth(true);
+	GetMesh()->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
+	
+	Weapon->SetRenderCustomDepth(true);
+	Weapon->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
 }
 
 void AAuraEnemy::UnHighlightActor()
 {
-	bDrawDebug = false;
+	GetMesh()->SetRenderCustomDepth(false);
+	GetMesh()->SetCustomDepthStencilValue(0);
+
+	Weapon->SetRenderCustomDepth(false);
+	Weapon->SetCustomDepthStencilValue(0);
 }
--- Source/AuraCourse/Public/Character/AuraEnemy.h
+++ Source/AuraCourse/Public/Character/AuraEnemy.h
@@ -16,11 +16,8 @@ class AURACOURSE_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterf
 	GENERATED_BODY()
 
 public:
+	AAuraEnemy();
 	virtual void HighlightActor() override;
 	virtual void UnHighlightActor() override;
-
-	UPROPERTY(BlueprintReadOnly)
-	bool bDrawDebug = false;
-	
 	
 };
