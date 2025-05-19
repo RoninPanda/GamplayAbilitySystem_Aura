944071ab46d1953a12da9da42b7a0ad9abbe5bfc
MatthewG <Matthewgraham375@gmail.com>
2025-05-19 12:49:09 -0400

    The Player State

---
+++ Source/AuraCourse/Public/Player/AuraPlayerState.h
@@ -0,0 +1,19 @@
+// Copyright Panda Productions
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "AuraPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURACOURSE_API AAuraPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+public:
+	AAuraPlayerState();
+	
+};
